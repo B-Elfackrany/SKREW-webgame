2c374aa281aeb26c0038e4faf0a72db90d3a07e6
B-Elfackrany <begad.elfackrany@gmail.com>
2024-09-29 19:01:32 +0400

    Implemented classes for player hand, game, and game room

--- Backend/main.java
+++ Backend/main.java
@@ -1,12 +1,28 @@
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 public class main {
     public static void main(String[] args) {
+        System.out.println(".");
+
         Deck deck = new ClassicDeck();
         deck.shuffleDeck();
-        deck.printDeck();
+        // deck.printDeck();
+
+        // System.out.println(deck.drawCard().cardInfo());
+
+        Hand hand = new Hand();
+        hand.initializeHand("Bigo", deck);
+        hand.printHand();
+        System.out.println(hand.getScore());
     }
+}
+enum Mode{
+    Classic,
+    Haramy,
+    TwoVTwo;
 }
 
 abstract class Card {
@@ -197,9 +213,11 @@ class RegularCard extends Card {
 
 abstract class Deck {
     protected Stack<Card> deck;
+    protected Stack<Card> playedPile;
 
     protected Deck() {
         deck = new Stack<>();
+        playedPile = new Stack<>();
         initializeDeck(); 
     }
 
@@ -207,6 +225,13 @@ abstract class Deck {
 
     public void shuffleDeck() {
         Collections.shuffle(this.deck);
+    }
+    public Card drawCard(){
+        if(!deck.isEmpty())
+        {
+            return deck.pop();
+        }
+        return null;
     }
 
     public void printDeck() { 
@@ -266,5 +291,117 @@ class TwoVTwoDeck extends ClassicDeck{
         deck.push(new PingCard());
         deck.push(new PongCard());
         deck.push(new PongCard());
+    }
+}
+
+class Hand{
+    protected List<Card> cards;
+    protected String playerID;
+    protected void initializeHand(String playerID, Deck deck)
+    {
+        cards= new ArrayList<>();
+        this.playerID = playerID;
+        cards.add(deck.drawCard());
+        cards.add(deck.drawCard());
+        cards.add(deck.drawCard());
+        cards.add(deck.drawCard());
+    }
+    public void printHand() {
+        System.out.println(playerID+"'s Hand:");
+        for (Card card : cards) {
+            System.out.println(card.cardInfo());  
+        }
+    }
+    public int getScore(){
+        int score = 0;
+        for(Card card: cards){
+            score += card.getScore();
+        }
+        return score;
+    }
+}
+
+abstract class Game {
+    protected Deck gameDeck;       
+    protected int numberOfPlayers;    
+    protected List<Hand> playerHands;  
+    protected int turnId;      
+    public Game(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+        this.gameDeck.initializeDeck();
+        this.playerHands = new ArrayList<>();
+        this.turnId = 0;  
+    }
+    protected void dealHands(){
+        for (int i = 0; i < numberOfPlayers; i++) {
+            Hand hand = new Hand();
+            hand.initializeHand(""+i, gameDeck);
+            playerHands.add(hand);
+        }
+    }
+    public Deck getGameDeck() {
+        return gameDeck;
+    }
+    public Hand getPlayerHand() {
+        return playerHands.get(turnId);
+    }
+    public Hand getPlayerHand(int PlayerId) {
+        return playerHands.get(PlayerId);
+    }
+    public void nextTurn() {
+        turnId = (turnId + 1) % numberOfPlayers;
+    }
+    public void printAllHands() {
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.println("Player "+i+"'s hand:");
+            playerHands.get(i).printHand();
+            System.out.println();
+        }
+    }
+}
+class ClassicGame extends Game {
+    public ClassicGame(int numberOfPlayers) {
+        super(numberOfPlayers);
+        this.gameDeck = new ClassicDeck(); 
+        this.gameDeck.initializeDeck();
+        dealHands();
+    }
+}
+class HaramyGame extends Game {
+    public HaramyGame(int numberOfPlayers) {
+        super(numberOfPlayers);
+        this.gameDeck = new HaramyDeck(); 
+        this.gameDeck.initializeDeck();
+        dealHands();
+    }
+}
+class TwoVTwoGame extends Game {
+    public TwoVTwoGame(int numberOfPlayers) {
+        super(numberOfPlayers);
+        this.gameDeck = new TwoVTwoDeck(); 
+        this.gameDeck.initializeDeck();
+        dealHands();
+    }
+}
+
+abstract class Room{
+    String roomId;
+    long hostSessionId;
+    int numberOfPlayers;
+    Mode mode ;
+    Game game;
+
+    protected Room(String roomId, long hostSessionId, int numberOfPlayers, Mode mode) {
+        this.roomId = roomId;
+        this.hostSessionId = hostSessionId;
+        this.numberOfPlayers = numberOfPlayers;
+        this.mode = mode;
+
+        switch (mode) {
+            case Classic -> this.game = new ClassicGame(numberOfPlayers);
+            case Haramy -> this.game = new HaramyGame(numberOfPlayers);
+            case TwoVTwo -> this.game = new TwoVTwoGame(numberOfPlayers);
+            default -> throw new IllegalArgumentException("Unexpected mode: " + mode);
+        }
     }
 }
